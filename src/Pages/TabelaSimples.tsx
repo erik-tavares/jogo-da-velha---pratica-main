d26a2407e07dbc95e9a08b74bbbbcc61dfe8c975
erik-tavares <erik_gabrieltavares@hotmail.com>
2022-07-06 22:50:43 -0300

    button add

--- src/Pages/TabelaSimples.tsx
+++ src/Pages/TabelaSimples.tsx
@@ -6,6 +6,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { FormControlLabel, FormGroup, Switch, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 type Row = {
     nome: string;
@@ -15,73 +17,94 @@ type Row = {
 
 const localStorageRows: Row[] = [
     {
-        nome: "erik",
+        nome: "Bread Pitt",
         jogadas: 10,
         tempo: '2:00'
     },
     {
-        nome: "Anderson",
+        nome: "Lola Pakita",
         jogadas: 12,
         tempo: '2:15'
     },
     {
-        nome: "Arthur",
+        nome: "Zack",
         jogadas: 15,
         tempo: '2:45'
     },
-    {
-        nome: "Erni",
-        jogadas: 15,
-        tempo: '2:45'
-    },
-    {
-        nome: "Rose",
-        jogadas: 15,
-        tempo: '2:45'
-    }
 ];
 
 // window.localStorage.setItem('ranking', JSON.stringify(localStorageRows))
 
-function comparar(a: Row, b: Row) {
-    return a.jogadas - b.jogadas
-}
+
 
 export default function TabelaSimples() {
+
+    function compararPorJogadas(a: Row, b: Row) {
+        return a.jogadas - b.jogadas
+    }
+
+    function compararPorTempo(a: Row, b: Row) {
+        return Number(a.tempo) - Number(b.tempo)
+    }
+
+    const [checked, setChecked] = React.useState(true);
+
     let rows: Row[] = JSON.parse(window.localStorage.getItem('ranking') || '[]')
-    rows.sort(comparar)
+    rows.sort(checked ? compararPorJogadas : compararPorTempo)
 
     return (
-        <TableContainer component={Paper} >
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Classificação</TableCell>
-                        <TableCell align="right">Nome</TableCell>
-                        <TableCell align="right">Jogadas</TableCell>
-                        <TableCell align="right">Tempo</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row, index) => {
+        <>
+
+            {/* <FormGroup>
+        <FormControlLabel control={<Switch
+          checked={checked}
+          onChange={(e)=>setChecked(e.target.checked)}
+          />} label="Ordenar classificação" />
+    </FormGroup> */}
+
+            <Box display='flex' flexDirection='column' alignItems='center'>
+                <Typography>Ordenar Classificação</Typography>
+                <Box display='flex' alignItems='center'>
+                    <Typography>Tempo</Typography>
+                    <Switch
+                        checked={checked}
+                        onChange={(e) => setChecked(e.target.checked)}
+                    />
+                    <Typography>Jogadas</Typography>
+                </Box>
+            </Box>
+
+            <TableContainer component={Paper} >
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Classificação</TableCell>
+                            <TableCell align="right">Nome</TableCell>
+                            <TableCell align="right">Jogadas</TableCell>
+                            <TableCell align="right">Tempo</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, index) => {
 
 
-                        return (
-                            <TableRow
-                                key={index + 1}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {index + 1}
-                                </TableCell>
-                                <TableCell align="right">{row.nome}</TableCell>
-                                <TableCell align="right">{row.jogadas}</TableCell>
-                                <TableCell align="right">{row.tempo}</TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                            return (
+                                <TableRow
+                                    key={index + 1}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {index + 1}
+                                    </TableCell>
+                                    <TableCell align="right">{row.nome}</TableCell>
+                                    <TableCell align="right">{row.jogadas}</TableCell>
+                                    <TableCell align="right">{Number(row.tempo) > 59 ? parseInt(`${(Number(row.tempo) / 60)}`) + ":" + `${Number(row.tempo) % 60}` : row.tempo}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
